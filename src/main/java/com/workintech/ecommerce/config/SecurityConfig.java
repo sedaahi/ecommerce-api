d130edbc3bf6b727878570f6fcfcb9ee166d0382
Seda Ahi <92363990+sedaahi@users.noreply.github.com>
2026-09-25 16:02:04 +0300

    Allow production frontend origin

--- src/main/java/com/workintech/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/workintech/ecommerce/config/SecurityConfig.java
@@ -75,7 +75,10 @@ public class SecurityConfig {
                 new CorsConfiguration();
 
         configuration.setAllowedOrigins(
-                List.of("http://localhost:5173")
+                List.of(
+                        "http://localhost:5173",
+                        "https://e-commerce-project-two-ashen.vercel.app"
+                )
         );
 
         configuration.setAllowedMethods(
